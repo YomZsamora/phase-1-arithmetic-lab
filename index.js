91ee144acba10c88e2f85b271a86732419eef5e5
yomZsamora <samora.y@adzumi.co.ke>
2022-02-02 16:17:30 +0300

    Wrote an equation that calculates a remainder that is equal to 4.

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ const multiply = num1 * num2;
 
 // Generates a random integer greater than 0. random should be a number
 let random = Math.floor(Math.random() * 10) + 1;
+
+//Wrote an equation that calculates a remainder that is equal to 4
+let num3 = 39;
+let num4 = 5;
+const mod = 39 % 5;
