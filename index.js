40dca687e02904cfc5aee893219fe81fdda5e7f2
yomZsamora <samora.y@adzumi.co.ke>
2022-02-02 16:16:54 +0300

    Generate a random integer greater than 0. random should be a number.

--- index.js
+++ index.js
@@ -3,3 +3,6 @@
 let num1 = 31;
 let num2 = 2;
 const multiply = num1 * num2;
+
+// Generates a random integer greater than 0. random should be a number
+let random = Math.floor(Math.random() * 10) + 1;
